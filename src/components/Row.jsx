010c564265de6e379097c1ab8325ac8c7c683da3
Kavuri Ram Mohan <rammohankavuri430@gmail.com>
2026-07-21 17:58:50 +0530

    Refactor Row component for better error handling

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,61 +1,110 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import tmdbAxiosInstance from '../tmdbAxiosInstance'
-import './Row.css'
-function Row({title,fetchUrl}) {
-const [allMovies,setAllMovies]=useState([])
-const base_url="https://image.tmdb.org/t/p/original/"
+import React, { useEffect, useState, useCallback } from "react";
+import tmdbAxiosInstance from "../tmdbAxiosInstance";
+import "./Row.css";
 
-const fetchData=useCallback(async()=>{
-   const {data}= await tmdbAxiosInstance.get(fetchUrl)
-   setAllMovies(data.results)
-},[fetchUrl])
+function Row({ title, fetchUrl }) {
+  const [allMovies, setAllMovies] = useState([]);
 
-useEffect(()=>{
-    fetchData()
-},[fetchData])
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
+  const fetchData = useCallback(async () => {
+    try {
+      const { data } = await tmdbAxiosInstance.get(fetchUrl);
+      setAllMovies(data.results || []);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [fetchUrl]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
-    <div className='row'>
-        <h1>{title}</h1>
-        <div className="all_movies">
+    <div className="row">
 
-            {
+      <h1>{title}</h1>
 
-                allMovies.map((item,index)=>(
-                    <div className='ba' key={item.id || index}>
-                       <div className='iim'> <img className='movie' src={`${base_url}/${item.poster_path}`} alt="" />
-                        
-                            <div className='back'>
-            
-                                    <img className='bacimg' src={`${base_url}/${item.backdrop_path}`} alt="" />
-                                    <div style={{padding: "10px"}}>
-                                        <div className='butt'>
-                                            
-                                              <button className='watchnow'>Watch now</button> <button className='plus'>+</button>                     
-        
-                                        </div>
-                                        <h2>{item.original_title}</h2>
-                                        <div style={{display:"flex"}}>
-                                            <h3>{item.release_date.slice(0,4)}</h3>
-                                            <h3> &nbsp;.&nbsp; </h3>
-                                            <h3>Rating: {item.vote_average}</h3>
-                                        </div>
-                                        <p>
-                                            {item.overview.slice(0,80)}
-                                        </p>
-                                    </div>
-            
-                                </div>
-                                </div>
-                        </div>
-                ))
-            }
-           
-        </div>
+      <div className="all_movies">
 
+        {allMovies.map((item) => (
+          <div className="ba" key={item.id}>
+
+            <div className="iim">
+
+              <img
+                className="movie"
+                src={`${base_url}${item.poster_path}`}
+                alt={item.original_title}
+              />
+
+              <div className="back">
+
+                <img
+                  className="bacimg"
+                  src={`${base_url}${item.backdrop_path}`}
+                  alt={item.original_title}
+                />
+
+                <div style={{ padding: "10px" }}>
+
+                  <div className="butt">
+
+                    <button className="watchnow">
+                      Watch now
+                    </button>
+
+                    <button className="plus">
+                      +
+                    </button>
+
+                  </div>
+
+
+                  <h2>
+                    {item.original_title}
+                  </h2>
+
+
+                  <div style={{ display: "flex" }}>
+
+                    <h3>
+                      {item.release_date
+                        ? item.release_date.slice(0, 4)
+                        : ""}
+                    </h3>
+
+                    <h3>
+                      &nbsp;.&nbsp;
+                    </h3>
+
+                    <h3>
+                      Rating: {item.vote_average}
+                    </h3>
+
+                  </div>
+
+
+                  <p>
+                    {item.overview
+                      ? item.overview.slice(0, 80)
+                      : ""}
+                  </p>
+
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+        ))}
+
+      </div>
 
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
